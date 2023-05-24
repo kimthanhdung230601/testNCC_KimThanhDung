4082543aa6a9cfeb881abd44d9bd483bfe306189
kimhanhung <kimthanhdung230601@gmail.com>
2023-05-24 20:17:31 +0700

    change layout

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import logo from './logo.svg';
-import Sidebar from './components/sidebar/sidebar';
-import Content from './components/content/content';
-import './App.css';
+import logo from "./logo.svg";
+import Sidebar from "./components/sidebar/sidebar";
+import Content from "./components/content/content";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Sidebar/>
-      <Content/>
+      <div className="row">
+        <div className="leftcolumn">
+          <Sidebar />
+        </div>
+        <div className="rightcolumn">
+          <Content />
+        </div>
+      </div>
     </div>
   );
 }
